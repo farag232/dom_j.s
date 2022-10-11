51204ed136a1299260da57635cfdbbfb6aa48982
farag232 <faragfadool232@gmail.com>
2022-10-11 23:43:42 +0300

    second message

---
+++ 19.2/for-each.js
@@ -0,0 +1,43 @@
+const users = [
+    {
+    id: 167464,
+    firstName: "Jimmy",
+    lastName: "Arnold",
+    email: "jimmya@gmail.com",
+    },
+    {
+    id: 578447,
+    firstName: "Martha",
+    lastName: "Goldman",
+    email: "gold@hotmail.com",
+    },
+    {
+    id: 864578,
+    firstName: "Tim",
+    lastName: "Burton",
+    email: "timmy.hotmail.com",
+    },
+];
+
+//! 1
+var orderList = document.createElement('ol');
+
+users.forEach((el)=>{
+    let li=document.createElement("li");
+    li.setAttribute("data-id" , `${el.id}`);
+    li.textContent=`${el.firstName} ${el.lastName}`;
+    orderList.append(li);
+})
+
+const list = document.querySelector(".list");
+list.append(orderList);
+console.dir(orderList);
+
+//!2 
+
+orderList.style.listStyleType="none";
+
+//! 3
+
+// i added this line to the 1 method. 
+// li.setAttribute("data-id" , `${Math.floor(Math.random() * 100)}`);
---
+++ 19.3/event.js
@@ -0,0 +1,51 @@
+const inputs = document.querySelectorAll(".inputs");
+let sub = document.querySelector("[paste-event]");
+
+//! paste and submit
+
+inputs.forEach((input)=>{
+    input.addEventListener('paste', (event)=>{
+        const paste = event.clipboardData.getData('text');
+        const data = paste.split('');
+        for(let i = 0; i<data.length; i++){
+            inputs[i].value = data[i];
+        }
+        // if(data.length==6) document.querySelector("#paste-event").submit();
+    })
+})
+
+//! autotabbing
+
+inputs.forEach((input, index)=>{
+    input.addEventListener('keyup',(event)=> {
+        if (input.nextElementSibling && input.value){
+            input.nextElementSibling.focus();
+        }  
+        let arr = [...inputs];
+        let allValues = arr.every(input=> input.value);
+        if (allValues) sub.submit();
+
+    });
+
+});
+//345678
+//! submit
+
+
+sub.addEventListener('submit',(event)=>{
+    var bool = arr.every(input=>{input.addEventListener('keyup',(event)=> (input.value))});
+     if (arr.every(input=> input.addEventListener('keyup',(event)=> (input.value)))) sub.submit();
+})
+
+
+extra 
+
+inputs.forEach((input)=>{
+    input.addEventListener('paste', (event)=>{
+        const paste = event.clipboardData.getData('text');
+        const data = paste.split('');
+        input.value = data;
+    })
+})
+
+//123456
